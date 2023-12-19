174ff16122d15eb64081565ddb6f9e377f1c1aa4
harsh dalal <dalalharsh1974@gmail.com>
2023-12-19 18:34:33 +0530

    home page header done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: '', component: LandingpageComponent },
+    { path: 'home', component: HomeComponent },
 
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatButtonToggleModule, FormsModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  tags: string[] = ['Tag1', 'Tag2', 'Tag3', 'Tag4'];
+  selectedTags: string[] = [];
 }
